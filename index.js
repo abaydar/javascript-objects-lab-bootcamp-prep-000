fc237fade5b8f08e8ef2b90044aa196f949f3242
bold-chain-4657 <amandabaydar@gmail.com>
2020-08-07 00:29:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 var recipes = new Object({cereal: "milk, cheerios", toast: "bread, butter"});
 function updateObjectWithKeyAndValue(recipes, food, ingredients){return Object.assign({}, recipes, {[food]: ingredients})}
+function destructivelyUpdateObjectWithKeyAndValue(recipes, food,ingredients){
